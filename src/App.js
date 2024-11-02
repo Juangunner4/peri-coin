c649492a6cafe2595b3c9564b20adeaaac2d7c13
Juan Vazquez <juanje1019@gmail.com>
2024-11-02 12:00:43 -0400

    Removing code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
-  const [activeMenu, setActiveMenu] = useState('Markets'); 
-
-  const toggleMenu = () => {
-    setActiveMenu(activeMenu === 'Markets' ? 'Socials' : 'Markets');
-  };
-
+  
   return (
     <Router>
       <div>
-        <Navbar/>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
