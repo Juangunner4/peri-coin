28c46af62596819067382b8a25fc382021f4c1fe
Juan Vazquez <juanje1019@gmail.com>
2024-11-05 09:56:08 -0500

    Game Teaser

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,6 +24,7 @@ const Navbar = () => {
         <ul>
           <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
           <li><Link to="/about" onClick={() => setIsOpen(false)}>About</Link></li>
+          <li><Link to="/" onClick={() => setIsOpen(false)}>Game</Link></li>
         </ul>
       </div>
     </nav>
