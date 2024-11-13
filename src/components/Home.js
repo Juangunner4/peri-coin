ced3441df77c30849576bfb0831cd1cae222c685
Juan Vazquez <juanje1019@gmail.com>
2024-11-13 00:28:06 -0500

    How to buy guide and disabled logo for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import logoCoin from '../images/logo.svg';
 import dexLogo from '../images/dexlogo.png';
 import '../styles/Home.css';
@@ -8,6 +8,7 @@ import { useTranslation } from 'react-i18next';
 
 function Home() {
   const [showCopied, setShowCopied] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
   const contractAddress = "EdopmgERFJbgJLVTwm9fuvt2Y5DmwjbjdZhVRrM3dpFd";
   const { t } = useTranslation();
 
@@ -23,18 +24,34 @@ function Home() {
     }
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 80) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="homeContainer">
-      <div className="logoCoin">
+      {/* <div className="logoCoin">
         <img src={logoCoin} alt="Logo Coin" />
-      </div>
+      </div> */}
 
       <div className="content">
         <p className="intro">
-        <span className="highlight">$Peri</span> {t("intro")} <span className="phrase">“Saquen el perico”</span>.
+          <span className="highlight">$Peri</span> {t("intro")} <span className="phrase">“Saquen el perico”</span>.
         </p>
         <span onClick={handleCopyAddress} className="contract-address">
-          {contractAddress}
+        {contractAddress}
         </span>
         {showCopied && (
           <div className="notification">Contract Copied</div>
@@ -52,6 +69,22 @@ function Home() {
           <a href="https://t.me/+rNFvjrSESP0yY2Ix" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faTelegramPlane} className="social-icon" />
           </a>
+        </div>
+        <div className={`guide ${isScrolled ? 'scrolled' : ''}`}>
+          <div className="guide-text">
+            <h2>{t("howtobuy")}</h2>
+            <ol>
+              <li>{t("guide_step1")}</li>
+              <li>{t("guide_step2")}</li>
+              <li>{t("guide_step3")}</li>
+              <li>{t("guide_step4")}</li>
+            </ol>
+          </div>
+          <div className="guide-images">
+            {/* <img src={step1Img} alt="Step 1" />
+            <img src={step2Img} alt="Step 2" />
+            <img src={step3Img} alt="Step 3" /> */}
+          </div>
         </div>
       </div>
     </div>
