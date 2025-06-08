13db45dcdff5e0c0fba2d5c93894f8d05233fcfc
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-06-08 18:41:49 -0400

    Improve drawer and manga section

--- src/pages/Manga.js
+++ src/pages/Manga.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import '../styles/Manga.css';
 import { useTranslation } from 'react-i18next';
+import * as Accordion from '@radix-ui/react-accordion';
 
 const Manga = () => {
   const { t } = useTranslation();
+  const items = [
+    'Perico Adventures - Chapter 5 releasing soon!',
+    'Crypto Feathers - New arc drops next week!',
+    'The Blockchain Bird - Volume 2 on the way!',
+  ];
+
   return (
     <div className="manga-page">
       <h2>{t('manga')}</h2>
-      <ul className="manga-list">
-        <li>Perico Adventures - Chapter 5 releasing soon!</li>
-        <li>Crypto Feathers - New arc drops next week!</li>
-        <li>The Blockchain Bird - Volume 2 on the way!</li>
-      </ul>
+      <Accordion.Root type="single" collapsible className="manga-list">
+        {items.map((item, index) => (
+          <Accordion.Item value={`item-${index}`} key={index} className="manga-item">
+            <Accordion.Header>
+              <Accordion.Trigger className="manga-trigger">{item}</Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="manga-content">{t('mangaComingSoon')}</Accordion.Content>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
+      <p className="fomo">{t('mangaFomo')}</p>
     </div>
   );
 };
